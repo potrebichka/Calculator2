16aba9dad57a7bb8e8e7320959a65c2edd9c5a5d
Nina Potrebich and Uriel Gonzalez <nina@>
2019-10-23 11:26:55 -0700

    Add more css styling and js updates.

--- JS/scripts.js
+++ JS/scripts.js
@@ -44,6 +44,30 @@ $(document).ready(function() {
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     var result = add(number1, number2);
-    $("#output").text(result);
+    $("#output").text(result).show();
+  });
+
+  $("form#substract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#sub1").val());
+    var number2 = parseInt($("#sub2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result).show();
+  });
+
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#mul1").val());
+    var number2 = parseInt($("#mul2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result).show();
+  });
+
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#div1").val());
+    var number2 = parseInt($("#div2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result).show();
   });
 });
